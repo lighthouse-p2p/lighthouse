aeff1c53917b2e75696c05fae999f432deb597bc
Akshit Garg <garg.akshit@gmail.com>
2020-12-12 00:46:56 +0530

    [Signal] Add 'from'

--- internal/models/signaling.go
+++ internal/models/signaling.go
@@ -2,6 +2,7 @@ package models
 
 // Signal is used to define the signal msg transmitted over the network
 type Signal struct {
-	To  string `json:"to"`
-	SDP string `json:"sdp"`
+	To   string `json:"to"`
+	From string `json:"from"`
+	SDP  string `json:"sdp"`
 }

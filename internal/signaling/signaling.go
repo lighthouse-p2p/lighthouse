a07ab7291507aa64617d361a36107da754cd38d3
Akshit Garg <garg.akshit@gmail.com>
2020-12-13 02:27:38 +0530

    [Logs] Remove unnecessary logging

--- internal/signaling/signaling.go
+++ internal/signaling/signaling.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"log"
 
 	"github.com/gorilla/websocket"
 	"github.com/lighthouse-p2p/lighthouse/internal/models"
@@ -108,11 +107,8 @@ func (c *Client) Listen() {
 					c.Chans[signal.From] = make(chan string, 5)
 				}
 
-				log.Println("Got answer")
-
 				c.Chans[signal.From] <- signal.SDP
 			} else if signal.Type == "o" {
-				log.Println("Got offer")
 				c.SignalChan <- signal
 			} else {
 				continue
@@ -123,7 +119,6 @@ func (c *Client) Listen() {
 
 // Push sends a message on the socket
 func (c *Client) Push(msg string) error {
-	log.Println("Pushed")
 	return c.Connection.WriteMessage(1, []byte(msg))
 }
 
